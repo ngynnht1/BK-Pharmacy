de031fb491ef84bb14fa5e6fdcfabd1d699baa83
Shane Nguyen <dungnguyen@Dungs-MacBook-Pro.local>
2021-12-04 19:38:53 +0700

    edit footer css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -54,13 +54,13 @@ const Footer = () => {
         <footer className="footer">
             <div className="container">
                 <Grid
-                    col={4}
+                    col={5}
                     mdCol={2}
                     smCol={1}
                     gap={10}
                 >
-                    <div>
-                    <iframe
+                    <div className="footer__map">
+                    <iframe className="footer__map__image"
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d979.8756236617819!2d106.69818758221511!3d10.772773001355503!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f54cba5ce41%3A0xfcf2a406f8355198!2sPharmacity!5e0!3m2!1svi!2s!4v1637220198289!5m2!1svi!2s"
                         title="Pharmacy shop"
                         width="300"
@@ -72,7 +72,7 @@ const Footer = () => {
                     </div>
                     <div>
                         <div className="footer__title">
-                            Về Pharmacy
+                            <h3>Về Pharmacy</h3>
                         </div>
                         <div className="footer__content">
                             {
@@ -88,7 +88,23 @@ const Footer = () => {
                     </div>
                     <div>
                         <div className="footer__title">
-                           Công Ty Cổ Phần Dược Phẩm Pharmacy
+                           <h3>Công Ty Dược Phẩm Pharmacy</h3>
+                        </div>
+                        <div className="footer__content">
+                            {
+                                footerCustomerLinks.map((item, index) => (
+                                    <p key={index}>
+                                        <Link to={item.path}>
+                                            {item.display}
+                                        </Link>
+                                    </p>
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <div>
+                        <div className="footer__title">
+                           <h3>Công Ty Dược Phẩm Pharmacy</h3>
                         </div>
                         <div className="footer__content">
                             {
@@ -109,10 +125,11 @@ const Footer = () => {
                                 <img src={logo} className="footer__logo" alt="" />
                             </Link>
                         </p>
-                        Tại Pharmacy, mỗi dược sỹ luôn tận tâm phục vụ và được đào tạo để hoàn thành xuất sắc những sứ mệnh được giao.
-                        <p>
+                        <div className="footer__content">
+                        <p> Tại Pharmacy, mỗi dược sỹ luôn tận tâm phục vụ và được đào tạo để hoàn thành xuất sắc những sứ mệnh được giao.</p>
+                        </div>
 
-                        </p>
+                       
                     </div>
                 </Grid>
             </div>
